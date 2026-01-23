c724f1298ef33ae3cd651d2cf1d671449cc56c1d
v-liuguangyuan <v-liuguangyuan@xiaomi.com>
2026-01-23 17:47:08 +0800

    feat: 修改正式环境请求配置

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,9 +1,11 @@
+const { protocol, hostname } = window.location
+
 export default {
-    mock: false,
-    nodeEnv: 'production',
-    useDomain: false,
-    host_official: 'https://www.olachat.sg',
-    api_help: 'https://help.olachat.sg',
-    api_api: 'https:///api.olachat.sg',
-    api_client: 'https://api.olachat.sg',
-};
+  mock: false,
+  nodeEnv: 'production',
+  useDomain: false,
+  host_official: protocol + '//' + hostname,
+  api_help: protocol + '//' + hostname,
+  api_api: protocol + '//' + hostname,
+  api_client: protocol + '//' + hostname,
+}
