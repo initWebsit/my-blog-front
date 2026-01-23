4af0d5b0290d53aafdcf91b8b0db7738798c9914
v-liuguangyuan <v-liuguangyuan@xiaomi.com>
2026-01-23 16:29:09 +0800

    fix: 修复引用目录名称错误的问题

--- src/projects/browser/publish/index.jsx
+++ src/projects/browser/publish/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import RichEditor from '@/components/RichEditor'
+import RichEditor from '@/components/richEditor'
 import { Picker, Toast } from '@/library/ui'
 import { getTags, publishBlog } from '@/network'
 
