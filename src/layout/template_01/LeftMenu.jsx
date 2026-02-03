66f4adadcf756b3a14caac0be5be037155b48abe
v-liuguangyuan <v-liuguangyuan@xiaomi.com>
2026-02-03 15:36:55 +0800

    feat: 更新备案

--- src/layout/template_01/LeftMenu.jsx
+++ src/layout/template_01/LeftMenu.jsx
@@ -9,14 +9,14 @@ function LeftMenu({ containerClassName }) {
     <div className={`left-menu ${containerClassName}`}>
       <Avatar />
       <div className='left-menu-footer'>
-        <a target='_blank' href='#' className='left-menu-footer-words'>
+        <a target='_blank' href='https://github.com/initWebsit/my-blog-front' className='left-menu-footer-words'>
           宁可错误的乐观，也不要正确的悲观。保持一颗勇敢的心。
         </a>
-        <a target='_blank' href='#' className='left-menu-footer-links'>
+        <a target='_blank' href='https://github.com/liuwanji/liuwanji.github.com/tree/master/html5' className='left-menu-footer-links'>
           回忆时刻
         </a>
         <a target='_blank' href='http://beian.miit.gov.cn' className='left-menu-footer-domain'>
-          苏ICP备20027520号-1
+          鄂ICP备2026006102号
         </a>
       </div>
     </div>
